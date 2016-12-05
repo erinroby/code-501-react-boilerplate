7d6ba6348e43c0e876488c64e2753efa602be79d
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-12-04 16:17:28 -0800

    class 1

--- app/components/MessagesList/MessagesList.js
+++ app/components/MessagesList/MessagesList.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import axios from 'axios'
 import MessageBox from './MessageBox'
+import _ from 'lodash'
 
 class MessagesList extends React.Component {
 
   constructor (props) {
     super(props)
     this.state = {
-      messageList: []
+      messageList: [],
+      token: ''
     }
   }
 
-  componentDidMount () {
-    axios.get('http://message-list.appspot.com/messages?limit=15')
+  getMessages () {
+    axios.get(`http://message-list.appspot.com/messages?limit=10&pageToken=${this.state.token}`)
     .then((res) => {
+      const oldMessages = this.state.messageList
+      const newMessages = oldMessages.concat(res.data.messages)
       this.setState({
-        messageList: res.data.messages
+        messageList: newMessages,
+        token: res.data.pageToken
       })
     })
     .catch((res) => {
@@ -23,11 +28,16 @@ class MessagesList extends React.Component {
     })
   }
 
+  componentDidMount () {
+    this.getMessages()
+  }
+
   render () {
     if (this.state.messageList.length > 0) {
+      const sortedMessages = _.orderBy(this.state.messageList, (o) => o.updated, 'desc')
       return (
         <ul className='MessagesList'>
-          {this.state.messageList.map((message, i) =>
+          {sortedMessages.map((message, i) =>
             <MessageBox
               key={i}
               author={message.author}
